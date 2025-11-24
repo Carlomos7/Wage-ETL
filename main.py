c117a52dfc6db77b0bb87a58b33d269e0c227c87
Carlos Segarra <cs525@njit.edu>
2025-11-24 13:57:07 -0500

    Refac: testing logging

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from config import get_settings
+from config.logging import setup_logging, get_logger
+
 def main():
+    setup_logging()
+    logger = get_logger(module=__name__)
+    logger.info("Starting the application")
     settings = get_settings()
-    print(settings.model_dump_json(indent=4))
+    logger.info(f"Settings: {settings.model_dump_json(indent=4)}")
 
 if __name__ == "__main__":
     main()
