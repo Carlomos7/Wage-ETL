ec025c86734e81edabf54344160541e006d58116
Carlos Segarra <cs525@njit.edu>
2025-11-25 14:15:44 -0500

    feat: add extract_table_headers function to parse table headers with colspan support

--- src/extract/web_scraper.py
+++ src/extract/web_scraper.py
@@ -35,3 +35,40 @@ def scrape_tables(page: requests.Response) -> list[BeautifulSoup]:
     logger.debug(f'Scraping tables from {page}')
     soup = BeautifulSoup(page.content, "html.parser")
     return soup.find_all('table', class_='results_table')
+
+
+def extract_table_headers(table: BeautifulSoup) -> list[str]:
+    '''
+    Extract the headers from the table
+    '''
+    logger.debug(f'Extracting headers from {table}')
+    theads = table.find_all('thead')
+    first_row = theads[0].find('tr')
+    adult_configs = []
+    for th in first_row.find_all('th'):
+        colspan = int(th.get('colspan', 1))
+        text = th.get_text(strip=True)
+        if text:
+            adult_configs.append((text, colspan))
+
+    second_row = theads[1].find('tr')
+    child_counts = []
+
+    for cell in second_row.find_all(['td', 'th']):
+        child_text = cell.get_text(strip=True)
+        child_counts.append(child_text)
+
+    headers = []
+    if child_counts[0]:
+        headers.append(child_counts[0])
+    else:
+        headers.append('Category')
+
+    col_index = 1
+    for adult_text, colspan in adult_configs:
+        for _ in range(colspan):
+            if col_index < len(child_counts):
+                headers.append(f'{adult_text} - {child_counts[col_index]}')
+                col_index += 1
+
+    return headers
