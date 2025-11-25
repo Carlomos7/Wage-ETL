768653e72c4926959bf85f839b606c11270e4895
Carlos Segarra <cs525@njit.edu>
2025-11-25 14:21:45 -0500

    Feat: Add scrape_county and scrape_all_counties functions to the extract module

---
+++ src/extract/__init__.py
@@ -0,0 +1,6 @@
+'''
+This module contains the extract functions for the application.
+'''
+from src.extract.web_scraper import scrape_county, scrape_all_counties
+
+__all__ = ["scrape_county", "scrape_all_counties"]
