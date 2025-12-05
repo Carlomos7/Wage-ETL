fa54a1ce10b889e38ffaff7275aa76baaa34a876
Carlos Segarra <cs525@njit.edu>
2025-12-05 10:26:58 -0500

    Refac: remove old extract architecture

--- src/extract/scrapers/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-Scraper implementations for different data sources.
-'''
-from src.extract.scrapers.base_scraper import WebScraperBase
-from src.extract.scrapers.wage_scraper import WageScraper
-from src.extract.caching.scraper_cache import ScraperCache
-
-__all__ = ["WebScraperBase", "WageScraper", "ScraperCache"]
